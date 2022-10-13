d11f880839a5f36c899c867240ad20a7880d5d67
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-10-13 19:09:13 +0400

    v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
+import { useState } from 'react'
+
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-
+      <h1>Register</h1>
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Name"
+        />
+        <br />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <br />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+      </form>
     </div>
   );
 }
